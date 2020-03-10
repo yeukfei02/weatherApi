4feec53b83b9321fc561b4c2cc8d93443b844d23
Donald Wu <donald.w@getzave.com>
2020-03-10 16:49:20 +0800

    fix-code: fix mongodb connect if is production

--- src/app.js
+++ src/app.js
@@ -20,13 +20,13 @@ if (environment === 'development') {
   mongoose.connect('mongodb://localhost:27017/weather', { useNewUrlParser: true, useUnifiedTopology: true });
 } else {
   // mongo atlas
-  mongoose.connect(
-    `mongodb+srv://yeukfei02:${process.env.MONGO_ATLAS_PASSWORD}@lunch-picker-goksl.mongodb.net/test?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-  );
+  // mongoose.connect(
+  //   `mongodb+srv://yeukfei02:${process.env.MONGO_ATLAS_PASSWORD}@lunch-picker-goksl.mongodb.net/test?retryWrites=true&w=majority`,
+  //   { useNewUrlParser: true, useUnifiedTopology: true },
+  // );
 
   // docker local mongodb
-  // mongoose.connect('mongodb://mongo:27017/lunch-picker', { useNewUrlParser: true, useUnifiedTopology: true });
+  // mongoose.connect('mongodb://mongo:27017/weather', { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 app.use(cors());
