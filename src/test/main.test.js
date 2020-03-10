7ed12f4a85fc432d6ddc387fc4d69b1becf1817e
Donald Wu <yeukfei02@gmail.com>
2020-03-10 21:27:52 +0800

    add-weather-route-#8: add test case

---
+++ src/test/main.test.js
@@ -0,0 +1,9 @@
+const weatherController = require('../controller/weather');
+
+describe('main.test', () => {
+  describe('weather', () => {
+    test('getWeather', () => {
+      expect(123).toBeDefined();
+    });
+  });
+});
